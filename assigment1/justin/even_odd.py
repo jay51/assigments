1bc881c882773ad185b1d0705ff89643f33c0089
jay51 <nenojafaar@gmail.com>
2023-11-06 21:08:04 -0600

    solution for task2 and 3

---
+++ assigment1/justin/even_odd.py
@@ -0,0 +1,21 @@
+"""""
+Task 2: Even or Odd
+Create a Python program that does the following:
+Ask the user to input a number.
+Check if the number is even or odd.
+Display a message to the user indicating whether the number is even or odd.
+"""
+
+def even_odd(num):
+    if num % 2 == 0:
+        print(f"The number you've entered, {num} is even.")
+    else:
+        print(f"The number you've entered, {num} is odd.")
+
+
+def main():
+    num = int(input("Enter a whole, no decimals, number. "))
+    even_odd(num)
+
+
+main()
---
+++ assigment1/justin/m_table.py
@@ -0,0 +1,28 @@
+"""
+Task 3: Multiplication Table
+Create a Python program that does the following:
+Ask the user to input a number.
+Generate & display the multiplication table for that number from 0 to 10.
+example:
+    1 x 0 = 0
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
+    1 x 4 = 4
+    ...
+"""
+
+
+def multiply(num):
+    for idx in range(11):
+        result = num * idx
+        print(f"{num} x {idx} = {result}.")
+
+
+
+
+def main():
+    num = int(input("Enter a whole numer, no decimals."))
+    multiply(num)
+
+main()
